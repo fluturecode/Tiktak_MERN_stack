b644d2b41ee1cc753578a514a6a9e630bfda9e19
fluturecode <fluturecode@gmail.com>
2020-09-02 17:47:49 -0400

    backend deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import axios from "./axios";
 import "./App.css";
 import { Video } from "./components/Video";
 
 function App() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		async function fetchPosts() {
+			const response = await Axios.get("/v2/posts");
+			setVideos(response.data);
+			return req;
+		}
+
+		fetchPosts();
+	}, []);
+
 	return (
 		<div className="app">
 			<div className="app__videos">
-				<Video />
-				<Video />
-				<Video />
+				{videos.map(({ url, channel, description, song, title }) => {
+					<Video url={url} channel={channel} />;
+				})}
 			</div>
 		</div>
 	);
---
+++ src/components/VideoFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function VideoFooter() {
+	return (
+		<div className="videoFooter">
+			<div className="videoFooter__text">
+				<h3>fluturecode</h3>
+			</div>
+		</div>
+	);
+}
+
+export default VideoFooter;
